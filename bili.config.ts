7de1385dd954cf6c7be39d560d1b6ef8087446cb
shinyaigeek <philispaxil@gmail.com>
2021-06-01 20:51:21 +0900

    Feature: PrefetchContainer -> PrefetchLink

---
+++ bili.config.ts
@@ -0,0 +1,7 @@
+import { Config } from 'bili'
+
+const config: Config = {
+  input: 'src/main.ts'
+}
+
+export default config
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
-class PrefetchContainer extends HTMLElement {
-    shadow: ShadowRoot;
-    constructor() {
-        super();
-
-        this.shadow = this.attachShadow({mode: 'open'});
-        this.shadow.innerHTML = `<slot></slot>`
+const observer = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        const entry = entries[entries.length - 1];
+        const prefetchLink = document.createElement("link");
+        prefetchLink.rel = "prefetch";
+        prefetchLink.as = "document";
+        prefetchLink.href = entry.target.getAttribute("href") ?? "";
+        document.head.appendChild(prefetchLink);
+        observer.unobserve(entry.target);
+      }
     }
+  },
+  { root: null, rootMargin: "0px", threshold: 0 }
+);
+
+class PrefetchAnchor extends HTMLAnchorElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    observer.observe(this);
+  }
+
+  disconnectedCallback() {
+    observer.unobserve(this);
+  }
 }
+
+customElements.define("prefetch-anchor", PrefetchAnchor, {
+  extends: "a",
+});
