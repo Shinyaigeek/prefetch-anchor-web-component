1f8d5cc6bd887cd05e61b582baac387d97e65f7a
shinyaigeek <philispaxil@gmail.com>
2021-06-03 02:08:45 +0900

    Feature: improve usability

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Be careful not to overuse
 <!DOCTYPE html>
 <html>
   <head>
-    <script src="https://cdn.jsdelivr.net/npm/prefetch-anchor-web-component@1.0.0/lib/main.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/prefetch-anchor-web-component@1.1.0/lib/main.js" ></script>
   </head>
   <body>
     hello prefetch anchor!!
@@ -22,7 +22,7 @@ Be careful not to overuse
 
 ## How to use
 
-1. define `prefetch-anchor` customElement via https://cdn.jsdelivr.net/npm/prefetch-anchor-web-component@1.0.0/lib/main.js with `<script />`
+1. define `prefetch-anchor` customElement via https://cdn.jsdelivr.net/npm/prefetch-anchor-web-component@1.1.0/lib/main.js with `<script />`
 2. add `prefetch-anchor` value to `is` attribute in `<a />`
 
-if you want to use this component also in legacy browser, I recommend to use https://cdn.jsdelivr.net/npm/prefetch-anchor-web-component@1.0.0/lib/main.old.js and use https://github.com/w3c/IntersectionObserver/tree/main/polyfill to polyfill IntersectionObserver API and use https://github.com/webcomponents/polyfills to polyfill customElement.
+if you want to use this component also in legacy browser, I recommend to use https://cdn.jsdelivr.net/npm/prefetch-anchor-web-component@1.1.0/lib/main.old.js and use https://github.com/w3c/IntersectionObserver/tree/main/polyfill to polyfill IntersectionObserver API and use https://github.com/webcomponents/polyfills to polyfill customElement.
--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,60 @@
-const observer = new IntersectionObserver(
-  (entries) => {
-    for (const entry of entries) {
-      if (entry.isIntersecting) {
-        const entry = entries[entries.length - 1];
-        const href = entry.target.getAttribute("href");
+(function () {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          const entry = entries[entries.length - 1];
+          const href = entry.target.getAttribute("href");
+          if (!!href) {
+            prefetch(href);
+          }
+          observer.unobserve(entry.target);
+        }
+      }
+    },
+    { root: null, rootMargin: "0px", threshold: 0 }
+  );
+
+  const isInViewPort: (el: HTMLElement) => boolean = function (el) {
+    const bounding = el.getBoundingClientRect();
+    return (
+      bounding.x >= 0 &&
+      bounding.y >= 0 &&
+      bounding.x <= window.innerWidth &&
+      bounding.y <= window.innerHeight
+    );
+  };
+
+  const prefetch: (href: string) => void = function (href) {
+    const prefetchLink = document.createElement("link");
+    prefetchLink.rel = "prefetch";
+    prefetchLink.as = "document";
+    prefetchLink.href = href;
+    document.head.appendChild(prefetchLink);
+  };
+
+  class PrefetchAnchor extends HTMLAnchorElement {
+    constructor() {
+      super();
+    }
+
+    connectedCallback() {
+      if (isInViewPort(this)) {
+        const href = this.getAttribute("href");
         if (!!href) {
           prefetch(href);
         }
-        observer.unobserve(entry.target);
+      } else {
+        observer.observe(this);
       }
     }
-  },
-  { root: null, rootMargin: "0px", threshold: 0 }
-);
 
-const isInViewPort: (el: HTMLElement) => boolean = function (el) {
-  const bounding = el.getBoundingClientRect();
-  return (
-    bounding.x >= 0 &&
-    bounding.y >= 0 &&
-    bounding.x <= window.innerWidth &&
-    bounding.y <= window.innerHeight
-  );
-};
-
-const prefetch: (href: string) => void = function (href) {
-  const prefetchLink = document.createElement("link");
-  prefetchLink.rel = "prefetch";
-  prefetchLink.as = "document";
-  prefetchLink.href = href;
-  document.head.appendChild(prefetchLink);
-};
-
-class PrefetchAnchor extends HTMLAnchorElement {
-  constructor() {
-    super();
-  }
-
-  connectedCallback() {
-    console.log(this, isInViewPort(this))
-    if (isInViewPort(this)) {
-      const href = this.getAttribute("href");
-      if (!!href) {
-        prefetch(href);
-      }
-    } else {
-      observer.observe(this);
+    disconnectedCallback() {
+      observer.unobserve(this);
     }
   }
 
-  disconnectedCallback() {
-    observer.unobserve(this);
-  }
-}
-
-customElements.define("prefetch-anchor", PrefetchAnchor, {
-  extends: "a",
-});
+  customElements.define("prefetch-anchor", PrefetchAnchor, {
+    extends: "a",
+  });
+})();
